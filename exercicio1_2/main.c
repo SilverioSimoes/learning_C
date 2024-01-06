d12f066626570e653c3d4faacf28cb9f6aa4ecd2
SilverioSimoes <silverio.simoes@gmail.com>
2024-01-05 23:59:59 -0300

    aula 31 e exercicio 2

---
+++ exercicio1_2/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+/*
+	2) Elabore um algoritmo que receba, por meio do teclado, dois valores,  um
+	para  a variável “a” e um para a variável “b”. Em seguida, faça os  passos
+	que julgar necessário para que ao final, a variável	“a” possua o valor que
+	inicialmente estava em “b” e a variável “b” possua o valor que inicialmente
+	estava em “a”. Traduza seu algoritmo para a linguagem C e exiba os valores
+	na tela.
+*/
+	int a, b;
+
+
+    printf("\nDigite um número inteiro para a variável \"a\": ");
+    scanf("%d", &a);
+    printf("\nDigite um número inteiro para a variável \"b\": ");
+    scanf("%d", &b);
+
+	a += b;
+	b = a - b;
+	a -= b;
+
+	printf("valor de \"a\": %d\tValor de \"b\": %d\n", a, b);
+
+    return 0;
+}
